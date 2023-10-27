5c3c7d5edd7d71077d3222d6ce72959dea6502d1
Janak singh dhami <info@jsdhami.com.np>
2023-10-28 00:46:08 +0545

    login form add

--- jshop/urls.py
+++ jshop/urls.py
@@ -5,5 +5,14 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("product-details", views.product_details, name="product-details"),
     path("login", views.login, name="login"),
+    path("signup", views.signup, name="signup"),
+    path("404", views.foferror, name="404"),
+    path("blog-single", views.blog_single, name="blog-single"),
+    path("blog", views.blog, name="blog"),
+    path("cart", views.cart, name="cart"),
+    path("checkout", views.checkout, name="checkout"),
+    path("shop", views.shop, name="shop"),
+    path("contact-us", views.contact_us, name="contact-us"),
+    
     
 ]
--- jshop/views.py
+++ jshop/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 # Create your views here.
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -9,12 +9,54 @@ def index(request):
 
     return render(request, 'index.html')
 
+
 def login(request):
     
     return render(request, 'login.html')
 
+def signup(request):
+    if request.method== 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        my_user = User.objects.create_user(username, email, password)
+        my_user.save()
+        return redirect('login')
+    
+    
+    return render(request, 'signup.html')
 
 
 def product_details(request):
     
     return render(request, 'product-details.html')
+
+def foferror(request):
+    
+    return render(request, '404.html')
+
+def blog(request):
+    
+    return render(request, 'blog.html')
+
+
+def blog_single(request):
+    
+    return render(request, 'blog-single.html')
+
+def cart(request):
+    
+    return render(request, 'cart.html')
+
+
+def checkout(request):
+    
+    return render(request, 'checkout.html')
+
+def shop(request):
+    
+    return render(request, 'shop.html')
+
+def contact_us(request):
+    
+    return render(request, 'contact-us.html')
